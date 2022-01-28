bc7afd35482390a9daeec5d45537284528be2f36
Henry <henryolson@icloud.com>
2022-01-28 17:17:56 -0600

    Built all the array functionality

--- Develop/script.js
+++ Develop/script.js
@@ -5,14 +5,13 @@
   let numberPrompt = "";
   let length = "";
   let passwordLength = 0;
-  let passwordAray = [];
-
-  const characters = [
+  let holdingSpot = [];
+  let characters = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
-    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 
-    'Z', '~', '!', '@', '#', '$', '%', '&', '+', '-', '?', '/', 1, 2, 3, 4, 5, 6, 7, 8, 9
-    ];  
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 1, 2, 3, 4, 5, 6, 7, 8, 9, '~', '!', '@', 
+    '#', '$', '%', '&', '+', '-', '?', '/', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
+    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
 
   function lengthPrompt() {
     length =  window.prompt("How many characters long would you like your password to be?");
@@ -41,9 +40,51 @@
 
     lengthPrompt()
     generatePrompts(upperCasePrompt)
+    if(!generatePrompts(upperCasePrompt)) {
+      characters = characters.slice(0, 45);
+    }
+
     generatePrompts(specialPrompt)
+    if(!generatePrompts(specialPrompt) && !generatePrompts(upperCasePrompt)) {
+      characters = characters.slice(0, 34);
+    }
+    else if(!generatePrompts(specialPrompt) && generatePrompts(upperCasePrompt)) {
+      holdingSpot = characters.slice(46, 71);
+      characters = characters.slice(0, 34);
+      characters = characters.concat(holdingSpot);
+    }
+
     generatePrompts(numberPrompt)
-  };
+    if(!generatePrompts(specialPrompt) && !generatePrompts(upperCasePrompt) && !generatePrompts(numberPrompt)) {
+      characters = characters.slice(0, 26);
+    }
+    else if(!generatePrompts(specialPrompt) && generatePrompts(upperCasePrompt) && !generatePrompts(numberPrompt)) {
+      holdingSpot = characters.slice(46, 71);
+      holdingSpot.unshift('L');
+      holdingSpot.unshift('K');
+      holdingSpot.unshift('J');
+      holdingSpot.unshift('I');
+      holdingSpot.unshift('H');
+      holdingSpot.unshift('G');
+      holdingSpot.unshift('F');
+      holdingSpot.unshift('E');
+      holdingSpot.unshift('D');
+      holdingSpot.unshift('C');
+      holdingSpot.unshift('B');
+      holdingSpot.unshift('A');
+      holdingSpot.push('Z');
+      characters = characters.slice(0, 25);
+      characters.push('z');
+      characters = characters.concat(holdingSpot);
+    }
+    else if(generatePrompts(specialPrompt) && !generatePrompts(upperCasePrompt) && !generatePrompts(numberPrompt)) {
+      holdingSpot = characters.slice(35, 45);
+      holdingSpot.push('/');
+      characters = characters.slice(0, 25);
+      characters.push('z');
+      characters = characters.concat(holdingSpot);
+    }
+  }
 
   function generatePassword() {
     prompts();
